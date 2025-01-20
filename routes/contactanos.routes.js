124cce352f426194798de6ca1e20d38f5dcef3fe
unknown <joaquin_marcelo@hotmail.com>
2025-01-20 11:21:08 -0500

    backend para contactanos

--- routes/contactanos.routes.js
+++ routes/contactanos.routes.js
@@ -1,9 +1,18 @@
 const express = require("express");
-const { saveContact } = require("../controllers/contactanos.controller");
+const { saveContact, getContacts, deleteContact, updateContactStatus } = require("../controllers/contactanos.controller");
 
 const router = express.Router();
 
 // Ruta para guardar contacto
-router.post("/contact", saveContact);
+router.post("/contacts", saveContact);
+
+// Ruta para obtener contactos con paginación
+router.get("/contacts", getContacts);
+
+// Ruta para eliminar un contacto por ID
+router.delete("/contacts/:id", deleteContact);
+
+// Ruta para actualizar el estado de un contacto (de 0 a 1)
+router.patch("/contacts/:id", updateContactStatus);
 
 module.exports = router;
