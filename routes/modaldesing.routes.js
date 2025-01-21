77baa99d63ae15a433402dc3ae749866b5a0aa32
JefersonDominguez <jeffersondominguez082@gmail.com>
2025-01-21 12:18:45 -0500

    APIS MODAL DESING

--- routes/modaldesing.routes.js
+++ routes/modaldesing.routes.js
@@ -4,9 +4,12 @@ const app = express()
 const controller = require("../controllers/modaldesing.controller")
 
 //Api para mandar toda la informacion de tipo GET con paginación de 20 en 20
-//Api para guardar información en el backend ( Nombre, Email, numero )
-//Api para eliminar un registro dependiendo del iD ue llegue del frontend por query
-
 app.get("/", controller.get)
+
+//Api para guardar información en el backend ( Nombre, Email, numero )
+app.post("/", controller.create);
+
+//Api para eliminar un registro dependiendo del iD ue llegue del frontend por query
+app.delete("/", controller.delete);
 
 module.exports = app
