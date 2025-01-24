0faf63b696e4443468f0d7f95c51c809b0f3ac9f
brayan anthony alaya atalaya <brayan.alaya@hotmail.com>
2025-01-24 10:05:44 -0500

    proyecto en laravel

---
+++ app/Http/Controllers/Api/ContactanosController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Contactanos;
+use Illuminate\Http\Request;
+
+class ContactanosController extends Controller
+{
+    public function get(){
+        return ;
+    }
+    public function create(){
+        return ;
+    }
+    public function update($id){
+        return ;
+    }
+    public function delete($id){
+        return ;
+    }
+}
---
+++ app/Http/Controllers/Api/ModalesController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ModalesController extends Controller
+{
+    public function get(){
+        return ;
+    }
+    public function create(){
+        return ;
+    }
+    public function delete(){
+        return ;
+    }
+}
