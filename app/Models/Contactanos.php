fad103cf0d4161e0d38094f886c8fef41cbb868f
unknown <joaquin_marcelo@hotmail.com>
2025-01-25 09:46:08 -0500

    Desarrollo del Api Contactanos

--- app/Models/Contactanos.php
+++ app/Models/Contactanos.php
@@ -2,10 +2,28 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Contactanos extends Model
 {
-    public $timestamps = false;
+    use HasFactory;
 
+    // Tabla asociada
+    protected $table = 'contactanos';
+
+    // Columnas rellenables
+    protected $fillable = [
+        'nombre',
+        'email',
+        'numero',
+        'mensaje',
+        'estado',
+    ];
+
+    // Ocultar columnas al retornar JSON (opcional)
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
 }
